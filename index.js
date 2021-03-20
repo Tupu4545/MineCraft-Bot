2608ab9f68fa29258452f85c8ecece35199a919f
sudo216 <55014913+sudo216@users.noreply.github.com>
2021-03-20 21:29:50 +0545

    upDAted!!!
    UPDATED!!!!

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
-const mineflayer = require('mineflayer');
-const discord = require('discord.js');
-const chalk = require('chalk');
-const client = new discord.Client();
-const config = require('./config.json');
-const bot = mineflayer.createBot({
-    host: config.host, 
-    port: config.port,
-    username: config.username
-})
-bot.on('login', async =>{
-    //bot.chat('/login password')//if there is any freaking passwords login :/
-    console.log('Iam in the server :D')
-})
+const mineflayer = require('mineflayer')
+const D = require('discord.js')
+const tpsPlugin = require('mineflayer-tps')(mineflayer)
+const client = new D.Client();
+const config = require('./config.json')
+const chalk = require('chalk')
+
+let prefix = config.prefix;
+let color = "#RANDOM";
+
+client.on('ready', activity => {
+  client.user.setStatus(`online`)
+  client.user.setActivity(
+    `Minecraft`, {
+      type: "PLAYING"
+    }
+  )
+});
+
 client.on('ready', async =>{
     console.log(chalk.redBright('l'))
     console.log(chalk.greenBright('o'))
@@ -23,26 +28,77 @@ client.on('ready', async =>{
     console.log(chalk.greenBright(` ${client.user.tag}`))
     console.log(chalk.greenBright('|- - - - - - - - - |'))
 })
-//receving msg
-bot.on("message", message => {
+
+let bot
+let isbotexist = false
+function turnonbot(ip, version, username, pass,client,msg) {
+if(isbotexist == true) return msg.channel.send(`There is already a bot running...\nUse ${prefix}leave first to get the bot off`)
+  bot = mineflayer.createBot({
+    host: ip,
+    username: username,
+    password: pass,
+    version: version
+  })
+  bot.loadPlugin(tpsPlugin)
+  bot.on('error', error =>{
+	let channel = client.channels.cache.get(config.channel)
+    if (!channel) return;
+    channel.send(`${error}`)
+  })
+  bot.on('kicked', kicked =>{
+	let channel = client.channels.cache.get(config.channel)
+    if (!channel) return;
+    channel.send(`${kicked}`)
+  })
+ 
+  bot.on('login', async () => {
+    console.log(chalk.magenta(`${bot.username} is on At ${ip}`))
+
+    let channel = client.channels.cache.get(config.channel)
+    channel.send(`${bot.username} Has Logged into ${ip}`)
+  })
+
+  bot.on("message", message => {
     let channel = client.channels.cache.get(config.channel)
     if (!channel) return;
     channel.send(`${message}`)
-})
-//sending msg
-client.on('message', msg => {
-    if (!msg.content.startsWith(config.prefix)) return
-    let args = msg.content.split(" ").slice(1)
-    args = msg.content.slice(config.prefix.length).split(/ +/);
-    let command = msg.content.split(" ")[0];
-    command = command.slice(config.prefix.length);
-    command = args.shift().toLowerCase();
-        if (command == "say"){
-            const chat = args.join(" ")
-            bot.chat(chat)
-        }
   })
-  client.login(config.token_discord)
+  isbotexist = true
+}
+function leave(msg){
+  isbotexist = false
+  bot.end()
+  bot.on("end", function(){
+    msg.channel.send("The Bot Has Sucsessfully ended")
+  })
+}
+client.on('message', msg => {
+  if (!msg.content.startsWith(prefix)) return
+  let args = msg.content.split(" ").slice(1)
+  args = msg.content.slice(prefix.length).split(/ +/);
+  let command = msg.content.split(" ")[0];
+  command = command.slice(prefix.length);
+  command = args.shift().toLowerCase();
+  if(command == "say") {
+    const chat = args.join(" ")
+    bot.chat(chat)
+  }
+  if(command == "tps") {
+	let channel = client.channels.cache.get(config.channel)
+	if (!channel) return;
+	channel.send('***Current tps: ***' + bot.getTps())
+  }
+  if(command == "join") {
+    // Command = {Prefix}join ip version username password
+    // Command = {Prefix}join args0 args1 args2 args3
+    turnonbot(args[0], args[1], args[2], args[3],client,msg)
+  }else if (command == "leave"){
+    leave(msg)
+  }
+})
+
+
+client.login(config.token_discord)
   .catch(error => {
-      console.log("Please Recheck The Token. [Can't Login]")
+    console.log(`cant login`);
   })
